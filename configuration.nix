7ffc416bbc484b1ff00bb129996f324b953cba2c
Aled Lorans <143277280+alorans@users.noreply.github.com>
2025-11-06 08:12:15 -0700

    SSH

--- configuration.nix
+++ configuration.nix
@@ -148,7 +148,14 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = true;
+      PermitRootLogin = "no";
+    };
+  };
+  networking.firewall.allowedTCPPorts = [ 22 ];
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
